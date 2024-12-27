b589e2900373440a3c67066ea5c56c4cfba74a54
Daniel Salazar <dsnile.salazr@gmail.com>
2024-12-26 22:36:03 -0600

    Añadir controladores y middleware de excepciones
    Se ha añadido la clase `BaseController` en el archivo `BaseController.cs`, que hereda de `ControllerBase` y está decorada con los atributos `[Route("api/[controller]")]` y `[ApiController]`.
    
    Se ha añadido la clase `MateriaController` en el archivo `MateriaController.cs`, que hereda de `BaseController`. Esta clase incluye:
    - Un campo privado `_repository` de tipo `IGenericRepository<Materia>`.
    - Un constructor que inicializa `_repository`.
    - Un método `GetAll` que maneja solicitudes HTTP GET y devuelve todos los registros de `Materia` utilizando `_repository`.
    - Un método `Create` que maneja solicitudes HTTP POST para crear una nueva instancia de `Materia` y la añade al repositorio. Si la inserción falla, lanza una excepción.
    
    Se ha añadido la clase `ExceptionMiddleware` en el archivo `ExceptionMiddleware.cs`, que maneja excepciones globales en la aplicación. Esta clase incluye:
    - Campos privados `_logger`, `_env` y `_next`.
    - Un constructor que inicializa estos campos.
    - Un método `InvokeAsync` que intenta ejecutar el siguiente middleware en la cadena y, en caso de una excepción, registra el error y establece el tipo de contenido de la respuesta a "application/json" y el código de estado a 500 (Internal Server Error).

---
+++ apisiase/Controllers/MateriaController.cs
@@ -0,0 +1,46 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apisiase.Controllers
+{
+
+    public class MateriaController : BaseController
+    {
+        private readonly IGenericRepository<Materia> _repository;
+
+        public MateriaController(IGenericRepository<Materia> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll() { 
+            
+            var records = await _repository.getAllAsync();
+
+            return Ok(records); 
+            
+        }
+
+
+        //[HttpGet("{id}")]
+       // public Task<IActionResult> GetByID(int id) { }
+
+        [HttpPost]
+       public async Task<ActionResult> Create(Materia materia) {
+        
+           var result = await _repository.add(materia);
+            if(result == 0)
+            {
+                throw new Exception("error al insertar");
+            }
+
+
+            return Ok(materia);
+        }
+
+
+    }
+}
---
+++ apisiase/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+namespace apisiase.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(ILogger<ExceptionMiddleware> logger, IHostEnvironment env, RequestDelegate next)
+        {
+            _logger = logger;
+            _env = env;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            }
+        }
+    }
+}
